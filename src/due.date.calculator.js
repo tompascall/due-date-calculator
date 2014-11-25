a248497a6742488aa2a57cd809df9c731087cacc
tompascall <tompascall@gmail.com>
2014-11-25 20:48:13 +0100

    check that submitdate is within working hours

--- src/due.date.calculator.js
+++ src/due.date.calculator.js
@@ -15,4 +15,8 @@ calc.DueDate.prototype.validateSubmitDate = function(submitDate){
   return submitDate instanceof Date;
 };
 
+calc.DueDate.prototype.checkWorkingHours = function(submitDate, startWorkingHours, endWorkingHours){
+  return submitDate.getHours() >= startWorkingHours && submitDate.getHours() <= endWorkingHours;
+};
+
 module.exports = calc;
--- test/due.date.calculator.spec.js
+++ test/due.date.calculator.spec.js
@@ -11,9 +11,18 @@ describe('Validation', function(){
     expect(due).to.be.an('object');
   });
 
-  it('should validate that submit date argument is a date object', function(){
+  it('should validate that submitDate argument is a date object', function(){
     var due = new calc.DueDate();
     var submitDate = new Date();
     expect(due.validateSubmitDate(submitDate)).to.equal(true);
+  });
+
+  it('should check that submitDate argument is within working hours', function(){
+    var startWorkingHours = 9;
+    var endWorkingHours = 17;
+    var due = new calc.DueDate();
+    var submitDate = new Date();
+    submitDate.setHours(10);
+    expect(due.checkWorkingHours(submitDate, startWorkingHours, endWorkingHours)).to.equal(true);
   });
 });
