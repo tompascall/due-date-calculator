786c69f7eeb0721d959752ba8ac8949b6fe6de25
tompascall <tompascall@gmail.com>
2014-12-01 13:15:41 +0100

    pass if we will clash with restingHours time frame

--- src/due.date.calculator.js
+++ src/due.date.calculator.js
@@ -9,10 +9,36 @@ calc.copyDate = function(src, dest){
 };
 
 calc.HandleFrames = function(timeFrames){
+ this.timeFrames = timeFrames.sort(this.compare);
+};
 
+calc.HandleFrames.prototype.compare = function(a, b){
+  if (a.priority > b.priority) return 1; // sort time frames by priority
+};
+
+calc.HandleFrames.prototype.isInFrame = function(frame, date){
+  var startDate = new Date();
+  var endDate = new Date();
+  calc.copyDate(date, startDate);
+  calc.copyDate(date, endDate);
+  if (frame.unit === 'hour') {
+    startDate.setHours(frame.start);
+    endDate.setTime(startDate.getTime() + frame.length);
+    if (date.getTime() > startDate.getTime() &&
+      date.getTime() < endDate.getTime()) {
+      return true;
+    }
+  }
+};
+
+calc.HandleFrames.prototype.setDateAFterFrame = function(date, frame){
+  date.setTime(date.getTime() + frame.length);
 };
 
 calc.HandleFrames.prototype.nextDate = function(date){
+  if (this.isInFrame(this.timeFrames[0], date)) {
+    this.setDateAFterFrame(date, this.timeFrames[0]);
+  }
   return date;
 };
 
@@ -25,7 +51,6 @@ calc.calculateDueDate = function(submitDate, turnaroundTime, timeFrames){
     dueDate.setHours(dueDate.getHours() + 1); // step to next hour
     dueDate = handleFrames.nextDate(dueDate);
   }
-  //console.log(dueDate.toString());
   return dueDate;
 };
 
--- test/due.date.calculator.spec.js
+++ test/due.date.calculator.spec.js
@@ -4,22 +4,24 @@
 
 var expect = require('expect.js');
 var calc = require('../src/due.date.calculator.js');
-var timeFrames = {
-  restingHours : {
-    unit : 'hour',
-    regularity : 'day',
-    start : 17,
-    length : 16,
-    priority : 1
-  },
-  weekend : {
+var timeFrames = [
+  {
+    name : 'weekend',
     unit : 'dayOfWeek',
     regularity : 'week',
     start : 6,
-    length : 2,
-    priority : 1
+    length : 2 * 24 * 60 * 60 * 1000,
+    priority : 1100
+  },
+  {
+    name : 'restingHours',
+    unit : 'hour',
+    regularity : 'day',
+    start : 17,
+    length : 16 * 60 * 60 * 1000,
+    priority : 1000
   }
-};
+];
 
 
 describe('due date calculation', function(){
@@ -34,6 +36,14 @@ describe('due date calculation', function(){
     var submitDate = new Date('December 5, 2014 15:00:00');
     var turnaroundTime = 2;
     var testDate = new Date('December 5, 2014 17:00:00');
+    var dueDate = calc.calculateDueDate(submitDate, turnaroundTime, timeFrames);
+    expect(dueDate.getTime()).to.equal(testDate.getTime());
+  });
+
+  it('should pass if we will clash with restingHours time frame', function(){
+    var submitDate = new Date('December 4, 2014 15:00:00');
+    var turnaroundTime = 3;
+    var testDate = new Date('December 5, 2014 10:00:00');
     var dueDate = calc.calculateDueDate(submitDate, turnaroundTime, timeFrames);
     expect(dueDate.getTime()).to.equal(testDate.getTime());
   });
