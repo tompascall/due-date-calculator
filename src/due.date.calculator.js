c5596d048859b940eb68745d28885d4cd0b94e38
tompascall <tompascall@gmail.com>
2014-12-09 22:08:46 +0100

    check that calculateDueDate() function has been called with at least 2 arguments

--- src/due.date.calculator.js
+++ src/due.date.calculator.js
@@ -8,8 +8,19 @@ calc.copyDate = function(src, dest){
   dest.setTime(src.getTime());
 };
 
-// calc.calculateDueDate = function(submitDate, turnaroundTime, timeFrames){
+calc.checkArgs = function(args){
+  var argArr = Array.prototype.slice.call(args);
+  if (argArr.length < 2) {
+    throw new Error('calculateDueDate() function must have at least 2 arguments');
+  }
+  var date = new Date();
+  if (!date.isPrototypeOf(argArr[0])) {
+    throw new Error('submitDate argument must be a date object');
+  }
+};
 
-// };
+calc.calculateDueDate = function(submitDate, turnaroundTime, timeFrames){
+  calc.checkArgs(arguments);
+};
 
 module.exports = calc;
--- test/due.date.calculator.spec.js
+++ test/due.date.calculator.spec.js
@@ -2,6 +2,26 @@
 
 'use strict';
 
-//var expect = require('expect.js');
-//var calc = require('../src/due.date.calculator.js');
+var expect = require('expect.js');
+var calc = require('../src/due.date.calculator.js');
+var helper = require('../lib/testHelpers.js');
+
+describe('Validate arguments', function(){
+  it('should check that calculateDueDate() function has been called with at least 2 arguments', function(){
+    var message = 'calculateDueDate() function must have at least 2 arguments';
+    var submitDate = new Date();
+    if (!helper.testExceptionMessage(message, calc.calculateDueDate, submitDate)) {
+      expect().fail();
+    }
+  });
+
+  it('should check that submitDate argument is a date object', function(){
+    var message = 'submitDate argument must be a date object';
+    var submitDate = 'not a date object';
+    var turnaroundtime;
+    if (!helper.testExceptionMessage(message, calc.calculateDueDate, submitDate, turnaroundtime)) {
+      expect().fail();
+    }
+  });
+});
 
